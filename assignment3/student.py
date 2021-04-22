d3295d90ab97e7203a1ee5cd7f52dc70561e47ee
Iain Pile <iainpile@pop-os.localdomain>
2021-04-22 18:40:03 -0400

    most fn impl

--- assignment3/student.py
+++ assignment3/student.py
@@ -1,6 +1,12 @@
 # Please place imports here.
 # BEGIN IMPORTS
-
+import numpy as np
+import scipy
+from scipy import ndimage
+import cv2
+import imageio
+import nose
+from nose.tools import set_trace
 # END IMPORTS
 
 
@@ -27,7 +33,17 @@ def compute_photometric_stereo_impl(lights, images):
         normals -- float32 height x width x 3 image with dimensions matching
                    the input images.
     """
-    raise NotImplementedError()
+    images = np.array(images)
+    N, h, w, c = images.shape
+    I_flat = np.reshape(images, (N, h*w*c))
+    G = np.linalg.inv(lights@lights.T)@lights@I_flat
+    G = np.reshape(G, (3, h, w, c))
+    ## update this to account for zeros above, this is causing a bug when calculating normals
+    albedo = np.linalg.norm(G, axis=0)
+    normals = np.sum(G, axis=3)
+    normals = normals / np.linalg.norm(normals, axis=0)
+    normals = np.transpose(normals, axes=(1, 2, 0))
+    return albedo, normals
 
 
 def pyrdown_impl(image):
@@ -58,7 +74,12 @@ def pyrdown_impl(image):
         down -- ceil(height/2) x ceil(width/2) x channels image of type
                 float32.
     """
-    raise NotImplementedError()
+    x_filter = np.expand_dims(1/16 * np.array([1, 4, 6, 4, 1]), axis=(0, 2))
+    y_filter = np.expand_dims(1/16 * np.array([1, 4, 6, 4, 1]), axis=(1, 2))
+    blurred_img = ndimage.convolve(image, x_filter, mode="mirror")
+    blurred_img = ndimage.convolve(blurred_img, y_filter, mode="mirror")
+    downsampled_image = blurred_img[::2, ::2]
+    return downsampled_image
 
 
 def pyrup_impl(image):
@@ -84,8 +105,19 @@ def pyrup_impl(image):
     Output:
         up -- 2*height x 2*width x channels image of type float32.
     """
-    raise NotImplementedError()
-
+    x_filter = np.expand_dims(1/8 * np.array([1, 4, 6, 4, 1]), axis=(0, 2))
+    y_filter = np.expand_dims(1/8 * np.array([1, 4, 6, 4, 1]), axis=(1, 2))
+    height = image.shape[0] * 2
+    width = image.shape[1] * 2
+    channels = image.shape[2]
+    upsized_img = np.zeros((height, width, channels))
+    height_indices = np.arange(0, image.shape[0], dtype=int) * 2
+    width_indices = np.arange(0, image.shape[1], dtype=int) * 2
+    for ind in height_indices:
+        upsized_img[ind][width_indices] = image[ind//2]
+    upscaled_img = ndimage.convolve(upsized_img, x_filter, mode="mirror")
+    upscaled_img = ndimage.convolve(upscaled_img, y_filter, mode="mirror")
+    return upscaled_img
 
 def project_impl(K, Rt, points):
     """
@@ -98,7 +130,11 @@ def project_impl(K, Rt, points):
     Output:
         projections -- height x width x 2 array of 2D projections
     """
-    raise NotImplementedError()
+    ones = np.ones((points.shape[0], points.shape[1], 1))
+    world_pts = np.concatenate((points, ones), axis=2)
+    P = K@Rt
+    img_pts = (world_pts @ P.T)[:, :, :2]
+    return img_pts
 
 
 def unproject_corners_impl(K, width, height, depth, Rt):
@@ -146,7 +182,20 @@ def unproject_corners_impl(K, width, height, depth, Rt):
     Output:
         points -- 2 x 2 x 3 array of 3D points
     """
-    raise NotImplementedError()
+    corner_pts = np.array([[[0, 0, 1], [width, 0, 1]], [[0, height, 1], [width, height, 1]]])
+    corner_pts = np.transpose(corner_pts, (0, 2, 1))
+    K_inv = np.linalg.inv(K)
+    camera_pts = depth * np.transpose((K_inv@corner_pts), (0, 2, 1))
+    camera_pts = np.append(camera_pts, np.array([[[1], [1]], [[1], [1]]]), axis=-1)
+    M = np.vstack((Rt, np.array([0, 0, 0, 1])))
+    M_inv = np.linalg.inv(M)
+    # R_prime = M_inv[:3, :3]
+    # Rt_prime = M_inv[:3, 3]
+    # p = (camera_pts @ R_prime) - Rt_prime
+    # print(p)
+    world_pts = (camera_pts@M_inv)
+    world_pts = world_pts[:, :, :3] / np.expand_dims(world_pts[:, :, 3], axis=2)
+    return world_pts
 
 
 def preprocess_ncc_impl(image, ncc_size):
@@ -196,7 +245,26 @@ def preprocess_ncc_impl(image, ncc_size):
     Output:
         normalized -- heigth x width x (channels * ncc_size**2) array
     """
-    raise NotImplementedError()
+    normalized = np.zeros((image.shape[0], image.shape[1], image.shape[2]*ncc_size**2))
+    pad = ncc_size // 2
+    padded_image = np.pad(image,
+                          pad_width=((pad, pad), (pad, pad), (0, 0)), mode="constant",
+                          constant_values=((0, 0), (0, 0), (0, 0)))
+    count = 0
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            patch = padded_image[i:i+ncc_size, j:j+ncc_size]
+            #patch -= np.expand_dims(np.mean(patch, axis=(0, 1)), axis=(0, 1))
+            flat_patch = np.zeros(image.shape[2]*ncc_size**2)
+            flat_patch[:ncc_size**2] = np.ndarray.flatten(patch[:, :, 0]) - np.mean(np.ndarray.flatten(patch[:, :, 0]))
+            flat_patch[ncc_size**2:2*ncc_size**2] = np.ndarray.flatten(patch[:, :, 1]) - np.mean(np.ndarray.flatten(patch[:, :, 1]))
+            flat_patch[2*ncc_size**2:3*ncc_size**2] = np.ndarray.flatten(patch[:, :, 2]) - np.mean(np.ndarray.flatten(patch[:, :, 2]))
+            n = np.linalg.norm(flat_patch)
+            if n < 1e-6:
+                normalized[i, j] = np.zeros(image.shape[2]*ncc_size**2)
+            else:
+                normalized[i, j] = flat_patch / n
+    return normalized
 
 
 def compute_ncc_impl(image1, image2):
